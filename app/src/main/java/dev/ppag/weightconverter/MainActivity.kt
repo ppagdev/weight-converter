a8210be087422c6735be58d5d51986dec5414c04
ppagdev <ppagdev@tuta.com>
2024-03-31 15:54:06 +0100

    added placeholder dropdown menu

--- app/src/main/java/dev/ppag/weightconverter/MainActivity.kt
+++ app/src/main/java/dev/ppag/weightconverter/MainActivity.kt
@@ -3,16 +3,18 @@ package dev.ppag.weightconverter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.runtime.*
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import dev.ppag.weightconverter.ui.theme.WeightConverterTheme
@@ -27,8 +29,47 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    InputField()
+                    Column {
+                        InputField()
+                        DropdownIn()
+                    }
                 }
+            }
+        }
+    }
+}
+
+@Composable
+fun DropdownIn(modifier: Modifier = Modifier) {
+    // temporary list of options
+    val options = listOf("Option 1", "Option 2", "Option 3")
+    var selectedOption by remember { mutableStateOf("") }
+    var isExpanded by remember { mutableStateOf(false) }
+
+    // Dropdown menu
+    Box {
+        DropdownMenu(
+            expanded = isExpanded,
+            onDismissRequest = { isExpanded = false },
+            modifier = modifier,
+        ) {
+            options.forEach { option ->
+                DropdownMenuItem(
+                    text = { Text(option) },
+                    onClick = {
+                        selectedOption = option
+                        // Handle user selection
+                        isExpanded = false
+                        // Perform action based on selected option
+                    }
+                )
+            }
+        }
+
+        // Button to activate dropdown menu
+        Row {
+            ElevatedButton(onClick = { isExpanded = true }) {
+                Text(text = selectedOption)
             }
         }
     }
@@ -53,6 +94,6 @@ fun InputField(modifier: Modifier = Modifier) {
 @Composable
 fun Preview() {
     WeightConverterTheme {
-        InputField()
+        DropdownIn()
     }
 }
