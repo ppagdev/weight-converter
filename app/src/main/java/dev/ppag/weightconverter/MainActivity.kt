9a885a164c688b8e74ecbdb96bd1c712c2953bea
ppagdev <ppagdev@tuta.com>
2024-03-31 21:58:11 +0100

    added basic conversion logic

--- app/src/main/java/dev/ppag/weightconverter/MainActivity.kt
+++ app/src/main/java/dev/ppag/weightconverter/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
@@ -14,9 +15,12 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.ppag.weightconverter.ui.theme.WeightConverterTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,8 +34,24 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Column {
-                        InputField()
-                        DropdownIn()
+                        // currently selected weight units
+                        var unitIn by remember { mutableStateOf(Wolverine) }
+                        var unitOut by remember { mutableStateOf(Human) }
+
+                        // output text value
+                        var outputText by remember { mutableStateOf("") }
+
+
+                        InputField({ outputText = unitIn.convertTo(unitOut).toString() })
+                        WeightTypeDropdown("in", unitIn, { unitIn = it }, { outputText = unitIn.convertTo(unitOut).toString() })
+                        ConvertButton()
+                        OutputField(outputText)
+                        WeightTypeDropdown("out", unitOut, { unitOut = it }, { outputText = unitIn.convertTo(unitOut).toString() })
+
+                        // debug, prints out when vars change
+                        LaunchedEffect(unitIn, unitOut) {
+                            println("Selected units: $unitIn, $unitOut")
+                        }
                     }
                 }
             }
@@ -40,10 +60,18 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun DropdownIn(modifier: Modifier = Modifier) {
+fun ConvertButton() {
+    ElevatedButton(onClick = {}) {
+        Icon(painter = painterResource(id = R.drawable.swap_horiz_fill0_wght400_grad0_opsz24),
+            contentDescription = "Swap Icon, for activating conversion")
+    }
+}
+
+@Composable
+fun WeightTypeDropdown(name: String, unit: WeightUnit, onUnitSelected: (WeightUnit) -> Unit, convert: () -> Unit, modifier: Modifier = Modifier) {
     // temporary list of options
-    val options = listOf("Option 1", "Option 2", "Option 3")
-    var selectedOption by remember { mutableStateOf("") }
+    val options = listOf(Human, Wolverine)
+    var selectedOption by remember { mutableStateOf(options[0]) }
     var isExpanded by remember { mutableStateOf(false) }
 
     // Dropdown menu
@@ -55,12 +83,13 @@ fun DropdownIn(modifier: Modifier = Modifier) {
         ) {
             options.forEach { option ->
                 DropdownMenuItem(
-                    text = { Text(option) },
+                    text = { Text(option.name) },
                     onClick = {
-                        selectedOption = option
                         // Handle user selection
+                        selectedOption = option
                         isExpanded = false
-                        // Perform action based on selected option
+                        onUnitSelected(option)
+                        convert()
                     }
                 )
             }
@@ -69,21 +98,30 @@ fun DropdownIn(modifier: Modifier = Modifier) {
         // Button to activate dropdown menu
         Row {
             ElevatedButton(onClick = { isExpanded = true }) {
-                Text(text = selectedOption)
+                Text(text = selectedOption.name)
             }
         }
     }
 }
 
 @Composable
-fun InputField(modifier: Modifier = Modifier) {
+fun OutputField(text: String, modifier: Modifier = Modifier) {
+    // display output
+    Row {
+        Text(text = text, modifier.padding(24.dp))
+    }
+}
+
+@Composable
+fun InputField(convert: () -> Unit, modifier: Modifier = Modifier) {
     // declare local variable for remembering user input while typing
     var text by remember { mutableStateOf("") }
 
     // display user input
     Row {
         OutlinedTextField(value = text,
-            onValueChange = { text = it },
+            onValueChange = {text = it
+                convert() },
             modifier = modifier,
             label = { Text(text = "Input") }
         )
@@ -94,6 +132,6 @@ fun InputField(modifier: Modifier = Modifier) {
 @Composable
 fun Preview() {
     WeightConverterTheme {
-        DropdownIn()
+        OutputField("Output")
     }
 }
