848712a49542ea0550192b15de50466629391043
ppagdev <ppagdev@tuta.com>
2024-04-04 12:35:51 +0100

    added some more units

--- app/src/main/java/dev/ppag/weightconverter/WeightUnit.kt
+++ app/src/main/java/dev/ppag/weightconverter/WeightUnit.kt
@@ -2,12 +2,15 @@ package dev.ppag.weightconverter
 
 // weight units used in the app
 val options = listOf(
-    WeightUnit("Kilograms", 1.0),
-    WeightUnit("Grams", 0.001),
-    WeightUnit("Pounds", 0.4535924),
-    WeightUnit("Cats", 4.535924),
-    WeightUnit("Humans", 62.0),
-    WeightUnit("Bananas", 0.118),
+    WeightUnit("Kilogram", 1.0),
+    WeightUnit("Gram", 0.001),
+    WeightUnit("Pound", 0.4535924),
+    WeightUnit("Cat", 4.535924),
+    WeightUnit("Human", 62.0),
+    WeightUnit("Banana", 0.118),
+    WeightUnit("Egg", 0.057),
+    WeightUnit("2007 Honda Civic", 1205.195),
+    WeightUnit("Sonic the Hedgehog", 35.0),
 ).sortedBy { it.name }
 
 class WeightUnit(val name: String, private val weight /* Kg */: Double) {
