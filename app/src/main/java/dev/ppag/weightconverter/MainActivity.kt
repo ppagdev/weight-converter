1771ddac56ae77fcf4d7728cdbb04d12af519906
ppagdev <ppagdev@tuta.com>
2024-04-04 12:10:12 +0100

    added swap button

--- app/src/main/java/dev/ppag/weightconverter/MainActivity.kt
+++ app/src/main/java/dev/ppag/weightconverter/MainActivity.kt
@@ -14,7 +14,9 @@ import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedIconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -27,6 +29,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -46,7 +49,7 @@ class MainActivity : ComponentActivity() {
                         TopTitleBar()
                     }
                     Column(
-                        verticalArrangement = Arrangement.spacedBy(78.dp, Alignment.CenterVertically),
+                        verticalArrangement = Arrangement.spacedBy(48.dp, Alignment.CenterVertically),
                         horizontalAlignment = Alignment.CenterHorizontally,
                     ) {
                         // currently selected weight units
@@ -75,7 +78,12 @@ class MainActivity : ComponentActivity() {
                                 Modifier.weight(1f))
                             WeightTypeDropdown(
                                 { unitIn = it },
-                                { outputText = unitIn.convertTo(unitOut, inputText).toString() })
+                                { outputText = unitIn.convertTo(unitOut, inputText).toString() },
+                                unitIn)
+                        }
+                        Row(verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.spacedBy(12.dp, Alignment.CenterHorizontally) ) {
+                            SwapButton({ val temp = unitIn; unitIn = unitOut; unitOut = temp },
+                            { outputText = unitIn.convertTo(unitOut, inputText).toString() },)
                         }
 
                         // output
@@ -83,13 +91,26 @@ class MainActivity : ComponentActivity() {
                             OutputField(outputText, Modifier.weight(1f))
                             WeightTypeDropdown(
                                 { unitOut = it },
-                                { outputText = unitIn.convertTo(unitOut, inputText).toString() })
+                                { outputText = unitIn.convertTo(unitOut, inputText).toString() },
+                                unitOut)
                         }
                     }
                 }
             }
         }
     }
+}
+
+@Composable
+fun SwapButton(swap: () -> Unit, convert: () -> Unit) {
+    OutlinedIconButton(onClick = { swap(); convert() },
+        content = {
+            Icon(
+                painter = painterResource(R.drawable.swap_horiz_fill0_wght400_grad0_opsz24),
+                contentDescription = "Swap Icon",
+                tint = MaterialTheme.colorScheme.onPrimaryContainer
+            )
+        })
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -99,9 +120,8 @@ fun TopTitleBar() {
 }
 
 @Composable
-fun WeightTypeDropdown(onUnitSelected: (WeightUnit) -> Unit, convert: () -> Unit, modifier: Modifier = Modifier) {
-    // temporary list of options
-    var selectedOption by remember { mutableStateOf(options[0]) }
+fun WeightTypeDropdown(onUnitSelected: (WeightUnit) -> Unit, convert: () -> Unit, selectedOption: WeightUnit,modifier: Modifier = Modifier) {
+//    var selectedOption by remember { mutableStateOf(options[0]) }
     var isExpanded by remember { mutableStateOf(false) }
 
     // Dropdown menu
@@ -116,7 +136,6 @@ fun WeightTypeDropdown(onUnitSelected: (WeightUnit) -> Unit, convert: () -> Unit
                     text = { Text(option.name) },
                     onClick = {
                         // Handle user selection
-                        selectedOption = option
                         isExpanded = false
                         onUnitSelected(option)
                         convert()
